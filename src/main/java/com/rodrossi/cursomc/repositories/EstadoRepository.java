e7584ce2c63f352bdcb075249a6e98becda7cef0
rodrossi <rodrigoormonde@gmail.com>
2019-11-28 09:00:16 -0300

    Estado e cidade

---
+++ src/main/java/com/rodrossi/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.rodrossi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrossi.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>  {
+	
+}
