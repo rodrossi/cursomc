095414b560dc654cb2d1d179b3d60bd0b45f30ee
rodrossi <rodrigoormonde@gmail.com>
2019-12-04 08:33:02 -0300

    validação Sintatica com Bean validation

--- src/main/java/com/rodrossi/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrossi/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrossi.cursomc.domain.Categoria;
+import com.rodrossi.cursomc.dto.CategoriaDTO;
 import com.rodrossi.cursomc.repositories.CategoriaRepository;
 import com.rodrossi.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrossi.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
